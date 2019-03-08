81d36d0c0d20d19fb8c1689b9cd9f03f26f6ce37
Adi <adi@ktds.co>
2019-03-08 18:55:07 +0530

    setup for scss and jsx

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -19,12 +20,37 @@ module.exports = {
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.scss$/,
+        include: path.join(__dirname, "/src/styles"),
+        loaders: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+         use: [
+           {
+             loader: 'file-loader',
+             options: {},
+           },
+         ],
+       },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html"
+    }),
+    new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        filename: "app.css",
+        chunkFilename: "bundle.css"
     })
   ]
 };
